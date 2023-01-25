3f5f6710af833bdaf16eec3092fcae1705f5b9cd
amit goldblatt <amit@goldblatt.in>
2023-01-25 14:38:12 +0200

    settings update

--- shopping/settings.py
+++ shopping/settings.py
@@ -81,10 +81,14 @@ WSGI_APPLICATION = 'shopping.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql',
+		'OPTIONS': {
+			'HOST': os.getenv('PGHOST'),
+			'USER': os.getenv('PGUSER'),
+			'PASSWORD': os.getenv('PGPASS'),
+		}
+	}
 }
 
 
